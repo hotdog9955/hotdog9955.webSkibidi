0f08a5620a02f0f5c800d37b1dddaf9b626df12f
Jordan Cisneros <45296453+hotdog9955@users.noreply.github.com>
2025-01-01 21:20:17 -0500

    Update main.gd
    fixed lagging toilet

--- main.gd
+++ main.gd
@@ -22,10 +22,13 @@ func _physics_process(dt):
 	if not is_instance_valid(playerData) or not is_instance_valid(PlayerAPI.local_player):
 		return
 	
-	var player:Actor = PlayerAPI.local_player
+	
+	var player:Actor = PlayerAPI.local_player  
 	var skeleton:Skeleton = player.get_node("body/player_body/Armature/Skeleton")
 	var torso_bone_index = skeleton.find_bone("torso")
 	var torso_global_transform = skeleton.global_transform * skeleton.get_bone_global_pose(torso_bone_index)
 	var offset = Transform(Basis(), Vector3(0, -0.5, 0.125))
 	var toilet = get_actor_from_id("toilet", player.owner_id)
-	if is_instance_valid(toilet): toilet.global_transform = torso_global_transform*offset
+	if not is_instance_valid(toilet): return
+	toilet.global_transform = torso_global_transform*offset
+	Network._send_P2P_Packet({"type": "actor_update", "actor_id": toilet.actor_id, "pos": toilet.global_transform.origin, "rot": toilet.rotation}, "peers", Network.CHANNELS.ACTOR_UPDATE)
