f51f50c66dd0bb006d819be6b91f04f8ccc02652
Jordan Cisneros <45296453+hotdog9955@users.noreply.github.com>
2025-01-01 20:17:17 -0500

    Update main.gd
    fixed for other players lol

--- main.gd
+++ main.gd
@@ -7,12 +7,12 @@ var PlayerAPI
 func _ready():
 	PlayerAPI = get_node_or_null("/root/BlueberryWolfiAPIs/PlayerAPI")
 
-func get_actor_from_id(id):
+func get_actor_from_id(type:String,ownerID):
 	var entities = get_node_or_null("/root/world/Viewport/main/entities")
 	if not entities: return null
 
 	for actor in entities.get_children():
-		if actor.actor_id == id:
+		if actor.actor_type == type and actor.owner_id == ownerID:
 			return actor
 
 	return null
@@ -22,15 +22,10 @@ func _physics_process(dt):
 	if not is_instance_valid(playerData) or not is_instance_valid(PlayerAPI.local_player):
 		return
 	
-	var player = PlayerAPI.local_player
+	var player:Actor = PlayerAPI.local_player
 	var skeleton:Skeleton = player.get_node("body/player_body/Armature/Skeleton")
 	var torso_bone_index = skeleton.find_bone("torso")
 	var torso_global_transform = skeleton.global_transform * skeleton.get_bone_global_pose(torso_bone_index)
 	var offset = Transform(Basis(), Vector3(0, -0.5, 0.125))
-	
-	for prop_data in playerData.props_placed:
-		if prop_data.ref == 781022529:
-			var toilet:Actor = get_actor_from_id(prop_data.id)
-			if not toilet:
-				return
-			toilet.global_transform = torso_global_transform*offset
+	var toilet = get_actor_from_id("toilet", player.owner_id)
+	if is_instance_valid(toilet): toilet.global_transform = torso_global_transform*offset
